68f4ea83b8b3347367bfd45f1d86c22299b74db9
RyotaOnodera124 <onoryo.0124@gmail.com>
2023-08-27 18:30:42 +0900

    Initial Commit

---
+++ syoryuken/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  syoryuken
+//
+//  Created by 小野寺良太 on 2023/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
