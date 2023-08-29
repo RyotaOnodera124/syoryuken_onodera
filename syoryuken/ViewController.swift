4257064912493e4e9f61919204a115e6b56a90af
RyotaOnodera124 <onoryo.0124@gmail.com>
2023-08-30 07:11:24 +0900

    昇龍拳おわったよ

--- syoryuken/ViewController.swift
+++ syoryuken/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var dispImageNo = 0
+    
+    var imageAttack : Array<UIImage> = []
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        for i in 1...20 {
+            if let attackImage = UIImage(named: "attack\(i)") {
+                imageAttack.append(attackImage)
+            }
+        }
+        
     }
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBAction func button(_ sender: Any) {
+        imageView.animationImages = imageAttack
+        //デュレーション(間隔)が2秒
+        imageView.animationDuration = 2
+        //一回繰り返す
+        imageView.animationRepeatCount = 1
+        //アニメーションを開始する
+        imageView.startAnimating()
+    }
 
 }
 
